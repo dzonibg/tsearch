ea55df383c2c9c1f5974f631b1284b46c84554ef
Nikola Stojisavljevic <nidza.bg@gmail.com>
2022-08-21 23:57:28 +0200

    Livewire + search API integration playground.

--- app/Repositories/SearchRepository.php
+++ app/Repositories/SearchRepository.php
@@ -5,7 +5,7 @@ namespace App\Repositories;
 class SearchRepository {
 
     public function sendSearchData($string) {
-        $response = \Http::get("https://tash-scraper.herokuapp.com/api/piratebay/$string");
+        $response = \Http::get(getenv('SEARCH_URL') . "api/piratebay/$string");
         $data = $response->json();
         return $data;
     }
