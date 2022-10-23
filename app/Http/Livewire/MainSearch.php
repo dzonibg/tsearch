80cbed5ccaf204dd50fc8395a7ed2c8bbe881116
Nikola Stojisavljevic <nidza.bg@gmail.com>
2022-10-23 22:01:05 +0200

    Added multi-page search. After initial response of first page, the rest of data will load. Weird issue with loading 2nd page on a method.

--- app/Http/Livewire/MainSearch.php
+++ app/Http/Livewire/MainSearch.php
@@ -10,12 +10,17 @@ class MainSearch extends Component
     public $torrents = [];
     public $string;
 
+    protected $listeners = [
+        'loadedFirstPage' => 'loadNextPages'
+    ];
+
     public function setString($string) {
         $this->string = $string;
     }
 
     public function fetch(SearchRepository $repository) {
         if (strlen($this->string) > 2) {
+            $this->emit("loadedFirstPage");
             $this->torrents = $repository->sendSearchData($this->string);
         } else {
             $this->torrents = (array) null;
@@ -24,11 +29,24 @@ class MainSearch extends Component
 
     public function updatedString() {
         if (strlen($this->string) > 2) {
+            \Log::info("Searching for $this->string");
             $repository = new SearchRepository();
+            $this->emit("loadedFirstPage");
             $this->torrents = $repository->sendSearchData($this->string);
         } else {
             $this->torrents = (array) null;
         }
+    }
+
+    public function firstPageLoaded() {
+        $this->loadNextPages();
+    }
+
+    public function loadNextPages() {
+        \Log::alert("Loading next pages.");
+        $repository = new SearchRepository();
+        $newData = $repository->fetchNextPages($this->string);
+        $this->torrents += $newData;
     }
 
     public function render()
--- app/Repositories/SearchRepository.php
+++ app/Repositories/SearchRepository.php
@@ -2,18 +2,48 @@
 
 namespace App\Repositories;
 
+use Illuminate\Http\JsonResponse;
+
 class SearchRepository {
 
     public array $data;
+    private int $objectsPerPage;
 
-    public function sendSearchData($string) {
+    public function fetchPage(string $string, int $pageNumber = 1): array {
+        \Log::info("Fetching Page $pageNumber for $string");
+        $response = \Http::get(getenv('SEARCH_URL') . "api/piratebay/$string/$pageNumber");
+        $data = $response->json();
+        return $data;
+    }
+
+    public function sendSearchData(string $string): array {
         return $this->fetchFirstPage($string);
     }
 
-    public function fetchFirstPage($string) {
-        $response = \Http::get(getenv('SEARCH_URL') . "api/piratebay/$string/1");
-        $data = $response->json();
+    public function fetchFirstPage(string $string): array {
+        $data = $this->fetchPage($string, 1);
         return $data;
+    }
+
+    public function fetchNextPages($string) { // method causes weird things to happen; need to load page 1 to fully load page 2? Will refactor this.
+        $currentPage = 1;
+        $errors = 0;
+        while ($errors == 0) {
+            \Log::info("Nextpage is $currentPage");
+            $pageData = $this->fetchPage($string, $currentPage);
+            if ((count($pageData) == 1) || ($currentPage > 30)) {
+                $errors = 1;
+                $endPage = $currentPage - 1;
+                \Log::info("Found $endPage pages for $string.");
+            } else {
+                // got items => push to array
+                foreach ($pageData as $datum) {
+                    $this->data[] = $datum;
+                }
+            }
+            $currentPage++;
+        }
+        return $this->data;
     }
 
 }
