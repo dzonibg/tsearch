fc84b8c1ba7fee2daa73555c3c43912d90e5f429
Nikola Stojisavljevic <nidza.bg@gmail.com>
2022-10-23 20:03:24 +0200

    Fixed issues caused by Clickboard API. Changing minimum PHP v to 7.4.

--- app/Repositories/SearchRepository.php
+++ app/Repositories/SearchRepository.php
@@ -4,8 +4,14 @@ namespace App\Repositories;
 
 class SearchRepository {
 
+    public array $data;
+
     public function sendSearchData($string) {
-        $response = \Http::get(getenv('SEARCH_URL') . "api/piratebay/$string");
+        return $this->fetchFirstPage($string);
+    }
+
+    public function fetchFirstPage($string) {
+        $response = \Http::get(getenv('SEARCH_URL') . "api/piratebay/$string/1");
         $data = $response->json();
         return $data;
     }
