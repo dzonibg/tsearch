52a61781602bc52524357777da686043169e58a7
Nikola Stojisavljević <33302164+dzonibg@users.noreply.github.com>
2023-06-21 08:01:34 +0200

    Update welcome.blade.php
    Changed DOM positioning for livewire assets.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,10 @@
       xmlns:main="http://www.w3.org/1999/html">
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-<head></head>
+<head>
+    @livewireStyles
+</head>
 <body>
-@livewireScripts
 @livewireStyles
 @include("toasts.mainToast")
 @include("toasts.copiedToast")
